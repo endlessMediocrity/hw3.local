cd886e03d23923705b4139d54dd1660cc406a3bf
badboy <112078908+endlessMediocrity@users.noreply.github.com>
2023-07-01 10:41:37 +0300

    in lesson

---
+++ in lesson/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="/in%20lesson/upload.phpn/upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="picture">
+    <button type="submit">Отправить</button>
+</form>
+
+</body>
+</html>
